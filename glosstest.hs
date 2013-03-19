820b28028b24a30acf3184b659ffbcf72d9d24cc
tomingtoming <tomingtoming@gmail.com>
2013-03-20 07:33:17 +0900

    define world

--- glosstest.hs
+++ glosstest.hs
@@ -2,18 +2,23 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
 main :: IO ()
-main = play (InWindow "Gloss Test" (640, 480) (10, 10)) white maxBound initWorld drawWorld eventWorld nextWorld
+main =
+  play (InWindow "Gloss Test" (640, 480) (10, 10))
+  white maxBound initWorld drawWorld eventWorld nextWorld
 
-data World = World Int
+data World = World [(String, Int, Int)]
 
 initWorld :: World
-initWorld = undefined
+initWorld = World [("Test", 100, 200)]
 
 drawWorld :: World -> Picture
-drawWorld world = undefined
+drawWorld (World elems) = pictures $ map drawElem elems
+
+drawElem :: (String, Int, Int) -> Picture
+drawElem (str,x,y) = translate (-170) (-20) $ text str
 
 eventWorld :: Event -> World -> World
-eventWorld event world = undefined
+eventWorld _ world = world
 
 nextWorld :: Float -> World -> World
-nextWorld float world = undefined
+nextWorld _ world = world
